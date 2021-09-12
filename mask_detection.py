a5c91753dea148f8a28d4b07b28addf0186c83a9
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-12 15:11:42 +0200

    code style linting

--- mask_detection.py
+++ mask_detection.py
@@ -4,19 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mediapipe as mp
 
-fixed_landmarks = [234, 454, 447, 345, 346, 347, 348, 349, 277, 437, 399, 419, 197, 196, 174, 47, 120, 119, 118, 117,
-                   116, 34, 127]
+fixed_landmarks = [234, 454, 447, 345, 346, 347, 348, 349, 277, 437, 399, 419,
+        197, 196, 174, 47, 120, 119, 118, 117, 116, 34, 127]
 
 def get_image(pathname):
     img = cv2.imread(pathname)
     img_height, img_width, img_channels = img.shape
     img_area = img_height*img_width
-    img = ResizeWithRatio(img,height=720)
+    img = resize_with_ratio(img,height=720)
     cv2.imshow('original', img)
     cv2.waitKey(0)
     return img
 
-def ResizeWithRatio(img, width=None, height=None, inter=cv2.INTER_AREA):
+def resize_with_ratio(img, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = img.shape[:2]
 
@@ -24,15 +24,15 @@ def ResizeWithRatio(img, width=None, height=None, inter=cv2.INTER_AREA):
         return img
     if width is None:
         r = height/float(h)
-        dim = (int(w*r),height)
+        dim = (int(w*r), height)
     else:
-        r = width /float(w)
-        dim = (width,int(h*r))
+        r = width/float(w)
+        dim = (width, int(h*r))
 
-    return cv2.resize(img,dim,interpolation=inter)
+    return cv2.resize(img, dim, interpolation=inter)
 
 # keypoint detection
-def find_facial_landmarks(img,landmarks=[],debug=False):
+def find_facial_landmarks(img, landmarks=[], debug=False):
 
     with open('landmarks_list.txt', 'r') as f:
         landmarks_list = [int(i) for i in f.readline().strip().split(',')]
@@ -49,7 +49,8 @@ def find_facial_landmarks(img,landmarks=[],debug=False):
     img_land = img.copy()
     if results.multi_face_landmarks:
         for faceLms in results.multi_face_landmarks:
-            mpDraw.draw_landmarks(img_land, faceLms,mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
+            mpDraw.draw_landmarks(img_land,
+                    faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
             #for id,lm in enumerate(faceLms.landmark):
             #    print(lm)
             #    ih, iw, ic = img.shape
@@ -58,23 +59,26 @@ def find_facial_landmarks(img,landmarks=[],debug=False):
         ih,iw,ic = img.shape
 
         for landmark in landmarks_list:
-            xc,yc =  int(faceLms.landmark[landmark].x*iw),int(faceLms.landmark[landmark].y*ih)
+            xc = int(facesLms.landmark[landmark].x*iw)
+            yc = int(facesLms.landmark[landmark].y*ih)
+
             if not landmark in fixed_landmarks:
                 yc += 40
             else:
                 yc -= 10
-            keypoints.append((xc,yc))
+            keypoints.append((xc, yc))
     if debug:
         cv2.imshow('landmarks', img_land)
         cv2.waitKey(0)
     return keypoints
 
 # color quantization on img with fixed number of bins
-def color_quantization(img,bins=2,debug=False):
-    Z = img.reshape((-1,3))
+def color_quantization(img, bins=2, debug=False):
+    Z = img.reshape((-1, 3))
     Z = np.float32(Z)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    ret,label,center=cv2.kmeans(Z,bins,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    ret, label, center = cv2.kmeans(Z, bins, None,
+            criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
     center = np.uint8(center)
     count_color = [0 for _ in range(bins)]
@@ -83,22 +87,23 @@ def color_quantization(img,bins=2,debug=False):
         count_color[elem[0]] += 1
     index_list = np.argsort(count_color)
     reference = center[index_list[-1]]
-    if (reference == [0,0,0]).all():
+    if (reference == [0, 0, 0]).all():
        reference = center[index_list[-2]]
 
     res = center[label.flatten()]
     res2 = res.reshape((img.shape))
     if debug:
-        cv2.imshow('kmeans',res2)
+        cv2.imshow('kmeans', res2)
         cv2.waitKey(0)
     return res2, reference
 
-# Function that given an img return a binary mask (np.array) of the surgical mask detected in the image img.
-# If facial landmarks are not detected, return an empty np.array.
-def find_mask(img,debug=False):
+# Function that given an img return a binary mask (np.array) of the surgical
+# mask detected in the image img. If facial landmarks are not detected, return
+# an empty np.array.
+def find_mask(img, debug=False):
 
     # Keypoints detection
-    keypoints = find_facial_landmarks(img,debug=debug)
+    keypoints = find_facial_landmarks(img, debug=debug)
     if not keypoints:
         return np.array([])
     # Creating mask to isolate surgical mask area
@@ -112,10 +117,10 @@ def find_mask(img,debug=False):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # Apply kmeans to find dominant color
-    res, reference = color_quantization(hsv,bins=3,debug=debug)
+    res, reference = color_quantization(hsv, bins=3, debug=debug)
 
     # Thresholding
-    res = res[:,:,2]
+    res = res[:, :, 2]
 
     hist = cv2.calcHist([res[not_black_pxl]], [0], None, [256], [0, 256])
     max_value_index = np.argmax(hist)
@@ -171,7 +176,7 @@ if __name__ == '__main__':
         print('[ERROR] Unable to detect facial landmarks')
         exit(1)
 
-    cv2.imshow('Mask',mask)
+    cv2.imshow('Mask', mask)
     cv2.waitKey(0)
 
 
