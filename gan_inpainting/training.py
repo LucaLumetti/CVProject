d0785ef840bf8bb8f50dc04f0a9ebabff2384f2d
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-16 00:50:19 +0200

    skeleton for training.py

---
+++ gan_inpainting/training.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+from generator import Generator
+from discriminator import Discriminator
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
