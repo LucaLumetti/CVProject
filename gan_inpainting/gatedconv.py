2020cd37e8dae28c7f08367d1ec61092f1b42c5f
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-15 17:26:03 +0200

    default for gateddeconv

--- gan_inpainting/gatedconv.py
+++ gan_inpainting/gatedconv.py
@@ -34,7 +34,17 @@ class GatedConv(nn.Module):
         return x
 
 class GatedDeConv(nn.Module):
-    def __init__(self, input_channels, output_channels, kernel_size, stride, padding, dilation, groups, bias, batch_norm, activation):
+    def __init__(self,
+            input_channels,
+            output_channels,
+            kernel_size,
+            stride=1,
+            padding=0,
+            dilation=0,
+            groups=1,
+            bias=True,
+            batch_norm=True,
+            activation=nn.LeakyReLU(0.2, inplace=True)):
         super(GatedDeConv, self).__init__()
         self.gatedconv = GatedConv(input_channels, output_channels, kernel_size, stride, padding, dilation, groups, bias, batch_norm, activation)
 
