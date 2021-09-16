a269cdaa15a7a21d5be8370d09fb45c08f7833fc
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-16 16:59:06 +0200

    basic structure of training

--- gan_inpainting/training.py
+++ gan_inpainting/training.py
@@ -7,6 +7,45 @@ import torch.nn as nn
 from generator import Generator
 from discriminator import Discriminator
 
+# a loss history should be held to keep tracking if the network is learning
+# something or is doing completely random shit
+# also a logger would be nice
+def train(netG, netD, optimG, optimD, lossG, lossD, dataloader):
+    netG.train()
+    netD.train()
+
+    for imgs, masks in dataloader:
+        netG.zero_grad()
+        netD.zero_grad()
+        optimG.zero_grad()
+        optimD.zero_grad()
+
+        # change img range from [0,255] to [-1,+1]
+        imgs = imgs / 127.5 - 1
+
+        # forward G
+        coarse_out, refined_out = netG(imgs, masks)
+        reconstructed_imgs = refined_out*masks + imgs*(1-masks)
+
+        true_imgs = torch.cat([imgs, masks, torch.full_like(masks, 1.)], dim=1)
+        gen_imgs = torch.cat([reconstructed_imgs, masks, torch.full_like(masks, 1.)], dim=1)
+        pos_neg_imgs = torch.cat([pos_imgs, neg_imgs], dim=0)
+
+        # forward D
+        pred_pos_neg_imgs = netD(pos_neg_imgs)
+        pred_pos_imgs, pred_neg_imgs = torch.chunk(pred_pos_neg_imgs, 2, dim=0)
+
+        # loss + backward D
+        loss_discriminator = lossD(pred_pos_imgs, pred_neg_imgs)
+        loss_discriminator.backward()
+        optimD.step()
+
+        # loss + backward G
+        loss_generator = lossG(pred_neg_imgs)
+        loss_generator.backward()
+        optimG.step()
+    return
+
 def main():
     ...
 
