e08fca098e22ac046565613f59dfcf1f713f6ddd
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-17 19:01:00 +0200

    logging and plotting during training

--- gan_inpainting/training.py
+++ gan_inpainting/training.py
@@ -6,6 +6,8 @@ from torchvision import transforms
 import torch.nn.functional as F
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+
 from generator import Generator
 from discriminator import Discriminator
 from loss import GeneratorLoss, DiscriminatorLoss, L1ReconLoss
@@ -19,7 +21,13 @@ def train(netG, netD, optimG, optimD, lossG, lossD, lossRecon, dataloader):
     netG.train()
     netD.train()
 
-    for imgs, masks in dataloader:
+    losses = {
+            'g': [],
+            'd': [],
+            'r': [],
+            }
+
+    for i, (imgs, masks) in enumerate(dataloader):
         netG.zero_grad()
         netD.zero_grad()
         optimG.zero_grad()
@@ -45,12 +53,14 @@ def train(netG, netD, optimG, optimD, lossG, lossD, lossRecon, dataloader):
 
         # loss + backward D
         loss_discriminator = lossD(pred_pos_imgs, pred_neg_imgs)
-        print(f'd_loss: {loss_discriminator}')
+        losses['d'].append(loss_discriminator)
 
         # loss + backward G
         loss_generator = lossG(pred_neg_imgs)
-        loss_generator += lossRecon(imgs, coarse_out, refined_out, masks)
-        print(f'g_loss: {loss_generator}')
+        loss_recon = lossRecon(imgs, coarse_out, refined_out, masks)
+        loss_gen_recon = loss_generator + loss_recon
+        losses['g'].append(loss_generator)
+        losses['r'].append(loss_recon)
 
         # these operations has been rearranged to avoid a autograd problem
         # not 100% sure on why retain_graph=True
@@ -59,10 +69,26 @@ def train(netG, netD, optimG, optimD, lossG, lossD, lossRecon, dataloader):
         # touch gradint that the generator has to use, then the first backward
         # needs to retain graph, the second does not, i will leave this comment
         # here for the future
-        loss_generator.backward()
+        loss_gen_recon.backward()
 
         optimD.step()
         optimG.step()
+        # every 100 img, print losses, update the graph, output an image as
+        # example
+        print(f"[{i}]\tloss_g: {losses['g'][-1]}, loss_d: {losses['d'][-1]}, loss_r: {losses['r'][-1]}")
+        if i%5 == 0:
+            fig, axs = plt.subplots(2, 1)
+            x_axis = range(len(losses['g']))
+            axs[0].plot(x_axis, losses['g'], x_axis, losses['r'])
+            axs[0].set_xlabel('it')
+            axs[0].set_ylabel('loss')
+            axs[1].plot(x_axis, losses['d'])
+            axs[1].set_xlabel('it')
+            axs[1].set_ylabel('loss')
+            axs[1].set_ylim(0,1)
+            fig.tight_layout()
+            fig.savefig('loss.png', dpi=fig.dpi)
+
     return
 
 if __name__ == '__main__':
