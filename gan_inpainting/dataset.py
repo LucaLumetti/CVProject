2553f4c6281153b4eaf84dccf9ea9037ac8c1b26
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-18 19:33:54 +0200

    fixing FaceMaskDataset and rewriting again FakeDataset to be able to test the code even without the real dataset

--- gan_inpainting/dataset.py
+++ gan_inpainting/dataset.py
@@ -9,10 +9,23 @@ from torchvision import io
 
 from torch.utils.data import Dataset, DataLoader
 
+class FakeDataset(Dataset):
+    def __init__(self):
+        return
+
+    def __len__(self):
+        return 10
+
+    def __getitem__(self, index):
+        return torch.rand((3, 256, 256)), torch.randint(0, 2, (1, 256, 256))
+
+    def loader(self, **args):
+        return DataLoader(self, **args)
+
 class FaceMaskDataset(Dataset):
     def __init__(self, csv_file, dataset_dir):
         self.dataset_dir = dataset_dir
-        self.images = pd.read_csv(csv_file)
+        self.images = pd.read_csv(csv_file, dtype='str')
 
     def __len__(self):
         return len(self.images)
@@ -21,10 +34,14 @@ class FaceMaskDataset(Dataset):
         if torch.is_tensor(index):
             index = index.tolist()
 
-        img_name = os.path.join(self.dataset_dir,
-                                self.images.iloc[index, 0])
+        # we expect the csv file format to be:
+        # index,path
+        # 0000,/a/0000.jpg
+        # 0001,/a/0001.jpg
+        # 0002,/b/0002.jpg
+        img_name = os.path.join(self.dataset_dir, self.images.iloc[index, 1])
         mask_name = img_name.split(".", 1)
-        mask_name = mask_name[0] + "_mask" + mask_name[1]
+        mask_name = mask_name[0] + "_mask." + mask_name[1]
         return (io.read_image(img_name), io.read_image(mask_name))
 
     def loader(self, **args):
