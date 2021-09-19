a046ddc5622037141240793b4dbacb9e4b71fe59
Luca Lumetti <lumetti.luca@gmail.com>
2021-09-19 19:23:29 +0200

    code style

--- script_dataset.py
+++ script_dataset.py
@@ -7,7 +7,7 @@ from mask_detection import find_facial_landmarks, get_image
 from pathlib import Path
 from PIL import Image
 
-def create_mask(img,debug=False):
+def create_mask(img, debug=False):
     keypoints = find_facial_landmarks(img)
     mask = np.zeros(img.shape[:2], np.uint8)
     cv2.fillPoly(mask, np.array([keypoints]), (255, 255, 255))
@@ -18,14 +18,14 @@ def create_mask(img,debug=False):
 
 def get_photos(pathname):
 
-    if not os.path.exists(pathname+"masked_images"):
-        os.mkdir(os.path.join(pathname,"masked_images"))
+    if not os.path.exists(pathname + "masked_images"):
+        os.mkdir(os.path.join(pathname, "masked_images"))
 
     directories = [f.stem for f in Path(pathname).glob("**/*") if f.is_dir() and f.resolve().stem != 'masked_images']
 
     for dir in directories:
-        if not os.path.exists(pathname+"masked_images/"+dir):
-            os.mkdir(os.path.join(pathname,"masked_images/"+dir))
+        if not os.path.exists(pathname + "masked_images/" + dir):
+            os.mkdir(os.path.join(pathname, "masked_images/" + dir))
 
     photos = [f for f in Path(pathname).glob("*/*") if f.is_file()]
 
@@ -39,9 +39,9 @@ if __name__ == '__main__':
         print(pathname)
         print(photo_index)
         img = cv2.imread(pathname)
-        cv2.imshow('source',img)
+        cv2.imshow('source', img)
         cv2.waitKey(0)
-        mask = create_mask(img,True)
+        mask = create_mask(img, True)
         im = Image.fromarray(mask)
-        im.save('dataset/masked_images/'+photo_index+'/'+photo_index+'_mask.png')
+        im.save('dataset/masked_images/' + photo_index + '/' + photo_index + '.png')
         break
