1bef94c3f6d9fef4f51d9189dd5ff196c1c98c42
Bartolosh <bartolosh@pc.fritz.box>
2021-09-24 17:49:58 +0200

    dataloader implementation

--- gan_inpainting/testing.py
+++ gan_inpainting/testing.py
@@ -1,16 +1,9 @@
 import sys
 
-import numpy as np
-
 import torch
 import cv2
-import torchvision
-from torchvision import transforms
-import torch.nn.functional as F
-import torch.nn as nn
 from config import Config
 from dataset import FaceMaskDataset
-from torchvision.utils import save_image
 
 from generator import Generator
 from discriminator import Discriminator
@@ -20,7 +13,7 @@ from script_dataset import create_mask
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def test(netG, netD, lossG, lossD, img, mask):
+def test(netG, netD, lossG, lossD, dataloader):
     netG.eval()
     netD.eval()
 
@@ -34,71 +27,59 @@ def test(netG, netD, lossG, lossD, img, mask):
         'd': []
     }
 
-    img = img.to(device)
-    mask = mask.to(device)
+    for i,(imgs,masks) in enumerate(dataloader):
 
-    img = img / 127.5 - 1
+        imgs = imgs.to(device)
+        masks = masks.to(device)
 
-    coarse_out, refined_out = netG(img,mask)
-    reconstructed_imgs = refined_out*mask + img*(1-mask)
+        imgs = imgs / 127.5 - 1
 
-    # forward G
-    pos_imgs = torch.cat([img, mask], dim=1)
-    neg_imgs = torch.cat([reconstructed_imgs, mask], dim=1)
-    pos_neg_imgs = torch.cat([pos_imgs, neg_imgs], dim=0)
+        coarse_out, refined_out = netG(imgs,masks)
+        reconstructed_imgs = refined_out*masks + imgs*(1-masks)
 
-    # forward D
-    pos_neg_imgs, dmasks = torch.split(pos_neg_imgs, (3, 1), dim=1)
-    pred_pos_neg_imgs = netD(pos_neg_imgs, dmasks)
-    pred_pos_imgs, pred_neg_imgs = torch.chunk(pred_pos_neg_imgs, 2, dim=0)
+        # forward G
+        pos_imgs = torch.cat([imgs, masks], dim=1)
+        neg_imgs = torch.cat([reconstructed_imgs, masks], dim=1)
+        pos_neg_imgs = torch.cat([pos_imgs, neg_imgs], dim=0)
 
-    # inference mode will be faster but can't make inplace op inside it
-    # Calculate accuracy and loss, dk if useful bc we're creating a separated file for that
-    with torch.no_grad():
-        mean_pos_pred = pred_pos_imgs.clone().detach().mean(dim=1)
-        mean_neg_pred = pred_neg_imgs.clone().detach().mean(dim=1)
-        mean_pos_pred[mean_pos_pred > 0.5] = 1
-        mean_pos_pred[mean_pos_pred <= 0.5] = 0
-        mean_neg_pred[mean_neg_pred > 0.5] = 0
-        mean_neg_pred[mean_neg_pred <= 0.5] = 1
-        accuracyD = torch.sum(mean_pos_pred) + torch.sum(mean_neg_pred)
-        accuracyD /= mean_pos_pred.shape[0] + mean_neg_pred.shape[0]
-        accuracies['d'].append(accuracyD.item())
+        # forward D
+        pos_neg_imgs, dmasks = torch.split(pos_neg_imgs, (3, 1), dim=1)
+        pred_pos_neg_imgs = netD(pos_neg_imgs, dmasks)
+        pred_pos_imgs, pred_neg_imgs = torch.chunk(pred_pos_neg_imgs, 2, dim=0)
 
-        # loss + backward D
-        loss_discriminator = lossD(pred_pos_imgs, pred_neg_imgs)
-        losses['d'].append(loss_discriminator.item())
+        # inference mode will be faster but can't make inplace op inside it
+        # Calculate accuracy and loss, dk if useful bc we're creating a separated file for that
+        with torch.no_grad():
+            mean_pos_pred = pred_pos_imgs.clone().detach().mean(dim=1)
+            mean_neg_pred = pred_neg_imgs.clone().detach().mean(dim=1)
+            mean_pos_pred[mean_pos_pred > 0.5] = 1
+            mean_pos_pred[mean_pos_pred <= 0.5] = 0
+            mean_neg_pred[mean_neg_pred > 0.5] = 0
+            mean_neg_pred[mean_neg_pred <= 0.5] = 1
+            accuracyD = torch.sum(mean_pos_pred) + torch.sum(mean_neg_pred)
+            accuracyD /= mean_pos_pred.shape[0] + mean_neg_pred.shape[0]
+            accuracies['d'].append(accuracyD.item())
 
-        # loss + backward G
-        pred_neg_imgs = netD(reconstructed_imgs, mask)
-        loss_generator = lossG(pred_neg_imgs)
-        loss_recon = lossRecon(img, coarse_out, refined_out, dmasks)
-        losses['g'].append(loss_generator.item())
-        losses['r'].append(loss_recon.item())
+            # loss + backward D
+            loss_discriminator = lossD(pred_pos_imgs, pred_neg_imgs)
+            losses['d'].append(loss_discriminator.item())
 
-        output = (reconstructed_imgs + 1) * 127.5
-        output = torch.flip(output,[-1])
+            # loss + backward G
+            pred_neg_imgs = netD(reconstructed_imgs, mask)
+            loss_generator = lossG(pred_neg_imgs)
+            loss_recon = lossRecon(img, coarse_out, refined_out, dmasks)
+            losses['g'].append(loss_generator.item())
+            losses['r'].append(loss_recon.item())
 
-    # TODO: need to save image somewhere, this is a temporary location
-    cv2.imwrite(config.test_dir+'/output/'+filename,output)
+            output = (reconstructed_imgs + 1) * 127.5
+            output = torch.flip(output,[-1])
+
+        # TODO: need to save image somewhere, this is a temporary location
+        cv2.imwrite(config.test_dir+'/output/'+filename,output)
     return
 
 if __name__ == '__main__':
     config = Config('config.json')
-    # Not sure if we really need dataloader in this case
-    #dataset = FaceMaskDataset(config.test_dir, 'maskffhq.csv')
-    #dataloader = dataset.loader(batch_size=config.batch_size)
-
-    netG = Generator(input_size=config.input_size).to(device)
-    netD = Discriminator(input_size=config.input_size).to(device)
-    netG.load_state_dict(torch.load(config.gen_path))
-    netD.load_state_dict(torch.load(config.disc_path))
-
-    lossG = GeneratorLoss()
-    lossRecon = L1ReconLoss()
-    lossD = DiscriminatorLoss()
-
-    # Calculate masks for all photos in test folder
     pathname = Path(sys.argv[1])
     split_folder = sys.argv[1].split('/')
     subfolder = split_folder[-2]
@@ -116,10 +97,23 @@ if __name__ == '__main__':
     mask = create_mask(img, False)
 
     if mask is not None:
-        #cv2.imwrite(str(path_to_mask), mask)
-        #csvf.write(f'{filename.split(".")[0]},{pathname.absolute()},{path_to_mask.absolute()}\n')
-        #csvf.close()
-        test(netG, netD, lossG, lossD, img, mask)
+        cv2.imwrite(str(path_to_mask), mask)
+        csvf.write(f'{filename.split(".")[0]},{pathname.absolute()},{path_to_mask.absolute()}\n')
+        csvf.close()
+
+        dataset = FaceMaskDataset(config.test_dir, 'maskffhq.csv')
+        dataloader = dataset.loader(batch_size=config.batch_size)
+
+        netG = Generator(input_size=config.input_size).to(device)
+        netD = Discriminator(input_size=config.input_size).to(device)
+        netG.load_state_dict(torch.load(config.gen_path))
+        netD.load_state_dict(torch.load(config.disc_path))
+
+        lossG = GeneratorLoss()
+        lossRecon = L1ReconLoss()
+        lossD = DiscriminatorLoss()
+
+        test(netG, netD, lossG, lossD, dataloader)
 
     else:
         print(f"Face not found for {pathname}")
