01ee239b1529b3e4d3e6cbca8ff714ab7f270d20
Bartolosh <matteobartolosh@gmail.com>
2021-10-05 00:04:44 +0200

    commit for saving changes

--- gan_inpainting/testing.py
+++ gan_inpainting/testing.py
@@ -10,17 +10,14 @@ from generator import *
 from discriminator import Discriminator
 from loss import *
 from pathlib import Path
-from script_dataset import create_mask
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def test(netG, netD, lossG, lossD, dataloader):
+def test(netG, netD, lossRecon, lossTV, lossVGG, dataloader):
     netG.eval()
     netD.eval()
 
     losses = {
-        'g': [],
-        'd': [],
         'r': [],
         'tv': [],
         'perc': [],
@@ -65,75 +62,44 @@ def test(netG, netD, lossG, lossD, dataloader):
             accuracyD /= mean_pos_pred.shape[0] + mean_neg_pred.shape[0]
             accuracies['d'].append(accuracyD.item())
 
-            # loss D
-            loss_discriminator = lossD(pred_pos_imgs, pred_neg_imgs)
-            losses['d'].append(loss_discriminator.item())
-
             # loss G
             pred_neg_imgs = netD(reconstructed_imgs, masks)
-            loss_generator = lossG(pred_neg_imgs)
             loss_recon = lossRecon(imgs, coarse_out, refined_out, dmasks)
             loss_tv = lossTV(refined_out)
             loss_perc, loss_style = lossVGG(imgs,refined_out)
-            losses['g'].append(loss_generator.item())
             losses['r'].append(loss_recon.item())
             losses['tv'].apppend(loss_tv.item())
             losses['perc'].append(loss_perc.item())
             losses['style'].append(loss_style.item())
 
-        output = (reconstructed_imgs[0] + 1) * 127.5
-        save_image(output/255,f'{config.dataset_dir}/output/{filename}')
+        #TODO: call metrics update
+
+        if config.batch_size == 1:
+            torch.unsqueeze(reconstructed_imgs,0)
+
+        for img in range(config.batch_size):
+            output = (reconstructed_imgs[img] + 1) * 127.5
+            save_image(output/255,f'{config.dataset_dir}/output/{dataloader.dataset.samples[i]}')
     return
 
 if __name__ == '__main__':
     config = Config('config.json')
-    logging.basicConfig(filename='test_output.log',encoding='utf-8',level=logging.DEBUG)
-    logging.debug(config)
+    #logging.basicConfig(filename='test_output.log',encoding='utf-8',level=logging.DEBUG)
+    #logging.debug(config)
 
-    sys.path.append(config.script_dataset_dir)
+    dataset = FaceMaskDataset(config.dataset_dir, 'maskffhq_test.csv')
+    dataloader = dataset.loader(batch_size=config.batch_size)
 
-    pathname = Path(sys.argv[1])
-    split_folder = sys.argv[1].split('/')
-    subfolder = split_folder[-2]
-    filename = split_folder[-1]
-    csvf = open(f'{pathname.parent.parent.parent}/maskffhq_test.csv', 'a')
-    path_to_mask = Path(f'{pathname.parent.parent.parent}' + \
-                        f'/masked_images/{subfolder}/{filename}')
-    path_to_mask.parent.mkdir(parents=True, exist_ok=True)
+    netG = MSSAGenerator(input_size=config.input_size).to(device)
+    netD = Discriminator(input_size=config.input_size).to(device)
 
-    if path_to_mask.exists():
-        logging.info('Path to mask already exists !')
-        exit(0)
+    netG.load_state_dict(torch.load(config.gen_path))
+    netD.load_state_dict(torch.load(config.disc_path))
 
-    img = cv2.imread(str(pathname))
-    mask = create_mask(img, False)
-
-    if mask is not None:
-        cv2.imwrite(str(path_to_mask), mask)
-        csvf.write(f'{filename.split(".")[0]},{pathname.absolute()},{path_to_mask.absolute()}\n')
-        csvf.close()
-
-        dataset = FaceMaskDataset(config.dataset_dir, 'maskffhq_test.csv')
-        dataloader = dataset.loader(batch_size=config.batch_size)
-
-        netG = MSSAGenerator(input_size=config.input_size).to(device)
-        netD = Discriminator(input_size=config.input_size).to(device)
-
-        netG.load_state_dict(torch.load(config.gen_path))
-        netD.load_state_dict(torch.load(config.disc_path))
-
-        lossG = GeneratorLoss()
-        lossRecon = L1ReconLoss()
-        lossTV = TVLoss()
-        lossD = DiscriminatorLoss()
-        lossVGG = VGGLoss()
-        test(netG, netD, lossG, lossD, lossRecon, lossTV, lossVGG, dataloader)
-
-        # deleting csv file to avoid repeating test on same images
-        os.remove(config.csvf_path)
-
-    else:
-        logging.info(f"Face not found for {pathname}")
-        exit(-1)
-
+    lossG = GeneratorLoss()
+    lossRecon = L1ReconLoss()
+    lossTV = TVLoss()
+    lossD = DiscriminatorLoss()
+    lossVGG = VGGLoss()
+    test(netG, netD, lossRecon, lossTV, lossVGG, dataloader)
 
