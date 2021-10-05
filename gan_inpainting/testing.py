1dfd89444a2d9ab7e1987ec3e8b29bc24739744e
Luca Lumetti <lumetti.luca@gmail.com>
2021-10-05 20:17:42 +0200

    no_grad to speedup and use less memory

--- gan_inpainting/testing.py
+++ gan_inpainting/testing.py
@@ -26,39 +26,40 @@ def test(netG, netD, dataloader):
             'd': []
             }
 
-    for i, (imgs,masks) in enumerate(dataloader):
-        imgs = imgs.to(device)
-        masks = masks.to(device)
+    with torch.no_grad():
+        for i, (imgs,masks) in enumerate(dataloader):
+            imgs = imgs.to(device)
+            masks = masks.to(device)
 
-        imgs = imgs / 127.5 - 1
-        masks = masks / 1
+            imgs = imgs / 127.5 - 1
+            masks = masks / 1
 
-        # forward G
-        coarse_out, refined_out = netG(imgs,masks)
-        reconstructed_imgs = refined_out*masks + imgs*(1-masks)
+            # forward G
+            coarse_out, refined_out = netG(imgs,masks)
+            reconstructed_imgs = refined_out*masks + imgs*(1-masks)
 
-        pos_neg_imgs = torch.cat([imgs,reconstructed_imgs],dim=0)
-        dmasks = torch.cat([masks,masks],dim=0)
+            pos_neg_imgs = torch.cat([imgs,reconstructed_imgs],dim=0)
+            dmasks = torch.cat([masks,masks],dim=0)
 
-        # forward D
-        pred_pos_neg_imgs = netD(pos_neg_imgs, dmasks)
-        pred_pos_imgs, pred_neg_imgs = torch.chunk(pred_pos_neg_imgs, 2, dim=0)
+            # forward D
+            pred_pos_neg_imgs = netD(pos_neg_imgs, dmasks)
+            pred_pos_imgs, pred_neg_imgs = torch.chunk(pred_pos_neg_imgs, 2, dim=0)
 
-        # Calculate accuracy over the batch
-        mean_pos_pred = pred_pos_imgs.clone().detach().mean(dim=1)
-        mean_neg_pred = pred_neg_imgs.clone().detach().mean(dim=1)
-        mean_pos_pred = torch.where(mean_pos_pred > 0.5, 1, 0).type(torch.FloatTensor)
-        mean_pos_neg = torch.where(mean_pos_pred < 0.5, 0, 1).type(torch.FloatTensor)
-        accuracyD = torch.sum(mean_pos_pred) + torch.sum(mean_neg_pred)
-        accuracyD /= mean_pos_pred.shape[0] + mean_neg_pred.shape[0]
-        accuracies['d'].append(accuracyD.item())
+            # Calculate accuracy over the batch
+            mean_pos_pred = pred_pos_imgs.clone().detach().mean(dim=1)
+            mean_neg_pred = pred_neg_imgs.clone().detach().mean(dim=1)
+            mean_pos_pred = torch.where(mean_pos_pred > 0.5, 1, 0).type(torch.FloatTensor)
+            mean_pos_neg = torch.where(mean_pos_pred < 0.5, 0, 1).type(torch.FloatTensor)
+            accuracyD = torch.sum(mean_pos_pred) + torch.sum(mean_neg_pred)
+            accuracyD /= mean_pos_pred.shape[0] + mean_neg_pred.shape[0]
+            accuracies['d'].append(accuracyD.item())
 
-        # Calculate L1 loss over the batch
-        l1_mean = torch.mean(torch.abs(imgs - reconstructed_imgs))
-        metrics['l1'].append(l1_mean)
+            # Calculate L1 loss over the batch
+            l1_mean = torch.mean(torch.abs(imgs - reconstructed_imgs))
+            metrics['l1'].append(l1_mean)
 
-        output = (reconstructed_imgs[0] + 1) * 127.5
-        save_image(output/255, f'{config.output_dir}/{i}.png')
+            output = (reconstructed_imgs[0] + 1) * 127.5
+            save_image(output/255, f'{config.output_dir}/{i}.png')
     return metrics
 
 if __name__ == '__main__':
