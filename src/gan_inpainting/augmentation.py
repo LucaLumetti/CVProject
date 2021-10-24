602637f545d9625c45302b23d33b803189e8688d
Luca Lumetti <lumetti.luca@gmail.com>
2021-10-24 15:31:49 +0200

    fix

--- src/gan_inpainting/augmentation.py
+++ src/gan_inpainting/augmentation.py
@@ -363,7 +363,7 @@ class AugmentPipe(torch.nn.Module):
                 images = images * C[:, :, :3].sum(dim=2, keepdims=True) + C[:, :, 3:]
             else:
                 print('Image should be RGB (3 channels) or L (1 channel)')
-                images = C[: :num_channels, :num_channels] @ images + C[:, :num_channels, num_channels:]
+                images[:, :3, :] = C[:, :3, :3] @ images[:, :3, :] + C[:, :3, 3:]
 
             images = images.reshape([batch_size, num_channels, height, width])
 
