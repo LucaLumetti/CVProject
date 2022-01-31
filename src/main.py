eaf0deafd23309524c0aa5fe5bdaf67b9d8796b3
Luca Lumetti <lumetti.luca@gmail.com>
2022-01-31 01:40:56 +0100

    update

--- src/main.py
+++ src/main.py
@@ -16,16 +16,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     front = cv2.imread(args.input_front)
-    mask = find_mask(front, save_mask=f'{output_dir}/mask.jpg', debug=True)
+    mask = find_mask(front, save_mask=f'{output_dir}/mask.jpg')
     mask = mask[..., np.newaxis]
-    cv2.imwrite(f'{output_dir}/face.jpg', (1-mask)*front)
-    cv2.imwrite(f'{output_dir}/mask.jpg', mask*255)
 
     if args.input_lateral is not None:
         lateral = cv2.imread(args.input_lateral)
         warped = warp_face(front, lateral)
+        mask_warped = warped.copy()
+        mask_warped[warped.sum(-1) > 0] = 1
+        mask_warped = (1-mask_warped)*mask
+        cv2.imshow('mwarped', mask_warped*255)
+        cv2.waitKey(0)
 
         # put the warped face over the front one by following the mask
         result = (1-mask)*front + mask*warped
-        cv2.imwrite(f'{output_dir}/face_ref.jpg', (1-mask)*front)
+        cv2.imwrite(f'{output_dir}/face_ref.jpg', result)
+        cv2.imwrite(f'{output_dir}/mask_ref.jpg', mask_warped*255)
+    else:
+        cv2.imwrite(f'{output_dir}/face.jpg', (1-mask)*front)
+        cv2.imwrite(f'{output_dir}/mask.jpg', mask*255)
 
